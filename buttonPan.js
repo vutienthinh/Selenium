cbdb9ab5adda08fbffd344aeaf492612ef00d717
Vu Tien Thinh (RBVH/ETI41) <uiv1hc@bosch.com>
2020-04-15 23:55:45 +0700

    Add

--- buttonPan.js
+++ buttonPan.js
@@ -12,67 +12,56 @@ describe('Button Zooming Canvas', function () {
     driver = new Builder().forBrowser('chrome').build();
     actions = driver.actions({ bridge: true });
     // await driver.get('http://localhost:4200/?taskId=12d2aca9-68d0-11ea-936c-0a16f1679cc9');
-    await driver.get('http://localhost:4200/?taskId=faebaf49-6dd1-11ea-936c-0a16f1679cc9');
+    await driver.get('http://localhost:4200');
     // await driver.executeScript('var canvas = document.getElementById("canvas");var evt = document.createEvent("MouseEvents");evt.initEvent("mousewheel", true, true);evt.wheelDelta = 120;canvas.dispatchEvent(evt);');
     // await driver.executeScript('var canvas = document.getElementById("canvas");var evt = document.createEvent("MouseEvents");evt.initEvent("mousewheel", true, true);evt.wheelDelta = -120;canvas.dispatchEvent(evt);');
   });
 
-  // it('01 The image has not changed', async function () {
+  it('01 The image has not changed', async function () {
+    await driver.sleep(15000);
+    await driver.findElement(By.className('lower-canvas')).then(async (elm) => {
+      //click icon zoom in
+      await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]'))));
+      await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]')).click();
+      await actions.click(elm).perform();
+      // click icon pan
+      await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Pan"]'))));
+      await driver.findElement(By.xpath('//img[@ng-reflect-message="Pan"]')).click();
+      await actions.dragAndDrop(elm, { x: 10, y: 220 }).perform();
+      // // click icon zoom out
+      // await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]'))));
+      // await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click();
+      // // click on image to zoom out
+      // await actions.doubleClick(elm).perform();
+      // const zoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
+      // // console.log(zoomLevel);
+      // await assert.equal(zoomLevel, DEFAULT_LEVEL);
+    });
+
+  });
+
+  // it('02 The image has zoomed in', async function () {
+  //   const initialZoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
   //   await driver.sleep(15000);
+
   //   await driver.findElement(By.className('lower-canvas')).then(async (elm) => {
   //     // click icon zoom out
   //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]'))));
   //     await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click();
   //     // click on image to zoom out
-  //     await actions.click(elm).perform();
+  //     await actions.doubleClick(elm).perform();
+  //     //click icon zoom in
+  //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]'))));
+  //     await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]')).click();
+  //     // click on image to zoom in
+  //     await actions.doubleClick(elm).perform();
   //     const zoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
-  //     // console.log(zoomLevel);
-  //     await assert.equal(zoomLevel, DEFAULT_LEVEL);
+  //     console.log(zoomLevel, initialZoomLevel);
+  //     // level of zoom in is 0.4
+  //     await assert.equal(zoomLevel, 0.6);
   //   });
 
   // });
-
-  it('02 The image has zoomed in', async function () {
-    const initialZoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
-    await driver.sleep(15000);
-
-    await driver.findElement(By.className('lower-canvas')).then(async (elm) => {
-      // click icon zoom out
-      // await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]'))));
-      // await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]')).click();
-      // // click on image to zoom out
-      // await actions.click(elm).perform().then(async () => {
-      //   // click icon zoom in
-      //   await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]'))));
-      //   await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click();
-      //   // await driver.sleep(10000);
-      //   // click on image to zoom in
-      //   await actions.click(elm).perform();
-      //   const zoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
-      //   console.log(zoomLevel, initialZoomLevel);
-      //   // level of zoom in is 0.4
-      //   await assert.equal(zoomLevel, 0.6);
-      // });
-      await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]'))));
-      await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click().then(async () => {
-        // click on image to zoom out
-        await actions.click(elm).perform()
-        // click icon zoom in
-        await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]'))));
-        await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]')).click()
-        // await driver.sleep(10000);
-        // click on image to zoom in
-        await actions.click(elm).perform();
-        const zoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
-        console.log(zoomLevel, initialZoomLevel);
-        // level of zoom in is 0.4
-        await assert.equal(zoomLevel, 0.6);
-      });
-      // await driver.sleep(15000);
-
-    });
-
-  });
 
   // it('03 The image has zoomed back out', async function () {
   //   // const initialZoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
@@ -82,17 +71,17 @@ describe('Button Zooming Canvas', function () {
   //   // await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click();
   //   await driver.findElement(By.className('lower-canvas')).then(async (elm) => {
   //     // click on image to zoom out
-  //     // await actions.click(elm).perform();
+  //     // await actions.doubleClick(elm).perform();
   //     //click icon zoom in
   //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]'))));
   //     await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom In"]')).click();
   //     // click on image to zoom in
-  //     await actions.click(elm).perform();
+  //     await actions.doubleClick(elm).perform();
   //     //click icon zoom out
   //     await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]'))));
   //     await driver.findElement(By.xpath('//img[@ng-reflect-message="Zoom Out"]')).click();
   //     // click on image to zoom out
-  //     await actions.click(elm).perform();
+  //     await actions.doubleClick(elm).perform();
   //     const zoomLevel = await driver.executeScript('var canvas = document.querySelector(".lower-canvas");return canvas.__ngContext__["3"]["8"].zoomLevel;');
   //     // console.log(zoomLevel, initialZoomLevel);
   //     // level of zoom in is 0.4
--- scenario4.js
+++ scenario4.js
@@ -14,16 +14,17 @@ describe('Draw Canvas', function () {
     driver = new Builder().forBrowser('chrome').build();
     // await driver.get('http://localhost:4200/?taskId=12d2aca9-68d0-11ea-936c-0a16f1679cc9');
     // await driver.get('http://localhost:4200/?taskId=faebaf49-6dd1-11ea-936c-0a16f1679cc9');
+    // await driver.get('https://gojs.net/latest/extensions/PolygonDrawing.html');
     await driver.get('http://localhost:4200');
     // click YES
-    const actions = driver.actions({ bridge: true });
-    await driver.wait(until.elementLocated(By.xpath('//mat-radio-group/mat-radio-button[1]')), 10000);
-    // await driver.wait(until.elementIsVisible(driver.findElement(By.id('mat-radio-2'))));
-    await driver.findElement(By.xpath('//mat-radio-group/mat-radio-button[1]')).click();
-    // select Pedestrian option
-    await driver.findElement(By.xpath('//mat-select')).click();
-    await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//mat-option[2]'))));
-    await driver.findElement(By.xpath('//mat-option[2]')).click();
+    // const actions = driver.actions({ bridge: true });
+    // await driver.wait(until.elementLocated(By.xpath('//mat-radio-group/mat-radio-button[1]')), 10000);
+    // // await driver.wait(until.elementIsVisible(driver.findElement(By.id('mat-radio-2'))));
+    // await driver.findElement(By.xpath('//mat-radio-group/mat-radio-button[1]')).click();
+    // // select Pedestrian option
+    // await driver.findElement(By.xpath('//mat-select')).click();
+    // await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//mat-option[2]'))));
+    // await driver.findElement(By.xpath('//mat-option[2]')).click();
   });
 
   // it('01 Drawing instructions show up', async function () {
@@ -102,18 +103,25 @@ describe('Draw Canvas', function () {
     // await driver.findElement(By.xpath('//mat-option[2]')).click();
     // // click Draw button
     // // await driver.findElement(By.xpath('//app-draw-bbcl-widget')).click();
-    await driver.sleep(5000);
+    // await driver.sleep(2000);
+    // await driver.findElement(By.xpath('//canvas')).then(async (elm) => {
+      // await actions.move({x: 320, y: 120}).press().move({x: 650, y: 700}).press().release().perform();
+      // console.log(Origin.Origin.POINTER);
+    await driver.sleep(10000);
     await driver.findElement(By.className('lower-canvas')).then(async (elm) => {
-      await actions.dragAndDrop(elm, { x: 100, y: 140 }).perform();
-      // await driver.wait(until.elementLocated(By.id('submit-button')), 10000);
-      await driver.findElement(By.id('submit-button')).click();
-      start = new Date().getTime();
-      await driver.wait(until.elementLocated(By.className('lower-canvas')), 10).then(async (elm) => {
-        end = new Date().getTime();
-        let time = end - start;
-        console.log(start, end, time);
-        await assert.ok(time < TIME_OUT);
-      });
+      await actions.move({origin: elm}).press(Origin.Button.RIGHT).move({ x: -400, y: 10 , origin: Origin.Origin.POINTER}).press(Origin.Button.RIGHT).perform();
+      // await actions.move({origin: elm}).press().move({x: 320, y: 120}).press().move({x: 420, y: 120}).press(Origin.Button.RIGHT).perform();
+      // await actions.move({ x: 0, y: 400}).release().perform();
+      // await actions.dragAndDrop(elm, { x: -280, y: 50 }).perform();
+      // // await driver.wait(until.elementLocated(By.id('submit-button')), 10000);
+      // await driver.findElement(By.id('submit-button')).click();
+      // start = new Date().getTime();
+      // await driver.wait(until.elementLocated(By.className('lower-canvas')), 10).then(async (elm) => {
+      //   end = new Date().getTime();
+      //   let time = end - start;
+      //   console.log(start, end, time);
+      //   await assert.ok(time < TIME_OUT);
+      // });
     });
   });
 
